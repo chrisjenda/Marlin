55b97e3f59c143fb7f80dfe719850ca81faa50a2
Chris Jenda <chrisjenda@gmail.com>
2021-03-18 20:19:46 -0700

    MEATPACK-5ee1087
    (MEATPACK) Made Meatpack version independent

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.0.7.2" "-S6"
+#define SHORT_BUILD_VERSION "2.0.7.2" "-MEATPACK"
 
 /**
  * Verbose version identifier which should contain a reference to the location
